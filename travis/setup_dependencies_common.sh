a12aa1e95f640a472bebce81de5b9fc59768405d
David Hoese <david.hoese@ssec.wisc.edu>
2019-11-17 20:36:54 -0600

    Remove extra fluff in retry commands just to see what is wrong

--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -52,79 +52,19 @@ function retry_on_known_error() {
         # the event a conda solve takes a really long time (>10 min). If
         # there is no output on travis for that long, the job is cancelled.
         set +e
-        $@ > $_tmp_output_file 2>&1
+        $@
         _exitval="$?"
         # Keep the cat here...otherwise _exitval is always 0
         # even if the conda install failed.
-        cat $_tmp_output_file
+#        cat $_tmp_output_file
         set -e
-
-        # The hack below is to work around a bug in conda 4.7 in which a spec
-        # pinned in a pin file is not respected if that package is listed
-        # explicitly on the command line even if there is no version spec on
-        # the command line. See:
-        #
-        #   https://github.com/conda/conda/issues/9052
-        #
-        # The hacky workaround is to identify overridden specs and add the
-        # spec from the pin file back to the command line.
-        if [[ -n $(grep "conflicts with explicit specs" $_tmp_output_file) ]]; then
-            # Roll back the command than generated the conflict message.
-            # To do this, we get the most recent environment revision number,
-            # then roll back to the one before that.
-            # To ensure we don't need to activate, direct output of conda to
-            # a file instead of piping
-            _revision_file="revisions.txt"
-            conda list --revision > _revision_file
-            _current_revision=$(cat _revision_file | grep \(rev | tail -1 | cut -d' ' -f5 | cut -d')' -f1)
-            conda install --revision=$(( $_current_revision - 1 ))
-            _tmp_spec_conflicts=bad_spec.txt
-            # Isolate the problematic specs
-            grep "conflicts with explicit specs" $_tmp_output_file > $_tmp_spec_conflicts
-
-            # Do NOT turn the three lines below into one by putting the python in a
-            # $()...we need to make sure we stay in the shell in which conda is activated,
-            # not a subshell.
-            _tmp_updated_conda_command=new_command.txt
-            python ci-helpers/travis/hack_version_numbers.py $_tmp_spec_conflicts "$@" > $_tmp_updated_conda_command
-            revised_command=$(cat $_tmp_updated_conda_command)
-            echo $revised_command
-            # Try it; if it still has conflicts then just give up
-            $revised_command > $_tmp_output_file 2>&1
-            _exitval="$?"
-            # Keep the cat here...otherwise _exitval is always 0
-            # even if the conda install failed.
-            cat $_tmp_output_file
-            if [[ -n $(grep "conflicts with explicit specs" $_tmp_output_file) ]]; then
-                echo "STOPPING conda attempts because unable to resolve conda pinning issues"
-                rm -f $_tmp_output_file
-                return 1
-            fi
-        fi
 
         # If the command was sucessful, abort the retry loop:
         if [ "$_exitval" == "0" ]; then
             break
         fi
-
-        # The command errored, so let's check its output for the specified error
-        # strings:
-        if [[ $_n_retries -lt $RETRY_MAX ]]; then
-            # If a known error string was found, throw a warning and wait a
-            # certain number of seconds before invoking the command again:
-            for _error in $RETRY_ERRORS; do
-                if [ -n "$(grep "$_error" "$_tmp_output_file")" ]; then
-                    echo "WARNING: The comand \"$@\" failed due to a $_error, retrying." 1>&2
-                    _n_retries=$(($_n_retries + 1))
-                    _retry=true
-                    sleep $RETRY_DELAY
-                    break
-                fi
-            done
-        fi
+        echo "WARNING: The command \"$@\" failed due to a $_error, retrying." 1>&2
     done
-    # remove the temporary output file
-    rm -f "$_tmp_output_file"
     # Finally, return the command's exit code:
     return $_exitval
 }
